d21a8ddf2e04591e66d00ffc52b3b1f2f5227147
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2024-05-06 11:15:59 +0100

    passing of props data

---
+++ src/components/featured/Data.js
@@ -0,0 +1,17 @@
+import { IoMdFootball,IoIosTennisball } from "react-icons/io";
+
+export const Data = [
+    {
+        fixture:'Manchester - Chelsea',
+        prediction:'GG',
+        icon:<IoMdFootball/>
+    },
+    {
+        fixture:'Rafa Nadal - Djokovic',
+        prediction:'2',
+        icon:<IoIosTennisball />
+
+    }
+]
+
+
--- src/components/featured/Feature.jsx
+++ src/components/featured/Feature.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './feature.css'
 import { AiFillBank } from "react-icons/ai";
+import { Data } from './Data';
+
+
+
+
 export const Feature = () => {
+  console.log(Data);
   return (
     <div id='feature'>
         <AiFillBank size={40} color='pink'/>
+        <h2>FEATURED PICKS</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, reprehenderit corrupti? Sapiente blanditiis neque autem consectetur qui sunt labore? Nulla.</p>
+       <div className='feature-section'>
+        <div>
+
+        </div>
+        <div className='prediction'>
+          <h3>Manchester - Chelsea</h3>
+          <h3>1X</h3>
+          <AiFillBank size={20} className='icon'/>
+        </div>
+       </div>
         </div>
   )
 }
